c7ee6526d1837564df3175dd206f604a1ca4e893
Katrina Hill <khill365@gmail.com>
2022-05-17 10:42:04 -0700

    pushed up wrong version before. this is the correct version

--- api/pygirl.py
+++ api/pygirl.py
@@ -30,68 +30,6 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(message.encode())
 
-        # name = dic.get("id: 2")
-        # does dict.get id:2 exist if so, yes, otherwise no
-        # serverless function cannot have a counter, but you can incorporate counter here
-        # incorporate Vercel and Twillio
-        # make a request
-        # wait for a response
-        # respond
-        # can have a URL for each turn, response, and game status
-
-
-# class Game:
-#   print("hello")
-
-#   def __init__(self):
-#     pass
-
-#   def welcome_message(self):
-#     # the user_answer starts the game; at the beginning the user doesn't guess.
-#     print("Welcome to PyGirl! Want to play a word game?")
-#     response = input("(y)es to play or (n)o to decline")
-#     response.toLowerCase
-#     if response == "n":
-#       print("Maybe next time!")
-#       sys.exit()
-#     if response == "y":
-#       print("Let's play!")
-#     # some functionality to begin game
-
-#   def yes_to_play(self):
-#     # user_answer = "y"
-#     # randomize
-#     pass
-
-# if __name__ == "__main__":
-#     game = Game()
-#     game.welcome_message()
-#     game.yes_to_play()
-
-
-# # this is the answer we're looking for
-# correct_answer = "PYTHON"
-# # this is the number of tries left. Wrong letters.
-# remaining_tries = 10
-# # state of the game at the beginning
-# game_state = "IN PROGRESS"
-# while user_answer != correct_answer:
-#     # ask the user for another letter
-#     if guess not in correct_answer:
-#         remaining_tries = remaining_tries -1
-#     else:
-#         # update user_answer so it includes the correct letter guess
-# otherwise:
-#     if user_answer == correct_answer:
-#         game over
-#         game_state = "YOU WIN"
-#     if the snake is complete:
-#         game over
-#         game_state = "YOU LOSE"
-# if game_state != "IN PROGRESS":
-#     game over --> user or pygirl won or loss
-#         # go back to main menu (start another game, quit)
-
 
 class Game:
     def __init__(self):
@@ -117,7 +55,7 @@ class Game:
             19: "init",
             20: "exception",
         }
-        self.definition = {
+        self.definitions = {
             1: "function definition",
             2: "lambda definition",
             3: "methods definition",
@@ -165,6 +103,39 @@ class Game:
         else:
             return False
 
+    def show_snake(self, wrong_guesses):
+        snake_imgs = [
+            '''
+        "xxxx -=: xxxxx"''',
+            """
+                        ________
+                xxxx -=:___________  xxxxx""",
+            """
+                        ________/   /
+                xxxx -=:___________/ xxxxx""",
+            """
+                       \\
+                        \    /
+                ________/   /
+        xxxx -=:___________/ xxxxx""",
+            """
+                         _____
+                       /  0 0 \\
+                       \\
+                        \    /
+                ________/   /
+        xxxx -=:___________/ xxxxx""",
+            """
+                         _____
+                       /  0 0 \\
+                       \    --------<
+                        \    /
+                ________/   /
+        xxxx -=:___________/ xxxxx
+        """,
+        ]
+        print(snake_imgs[wrong_guesses])
+
     def play(self):
         # prompt to see if they want to play
         print("Do you want to play a game?")
@@ -176,11 +147,12 @@ class Game:
         # pick random word. Their guess must be 1 letter long.
         id = random.randint(1, len(self.questions))
         word = self.questions[id]
+        print(self.show_word_so_far(word, set()))
         # print(f"it's a secret: {word}")
         letters = set()
         # while game loop
         while True:
-            response = input("What is your guess? >")
+            response = input("\n What is your guess? >")
             # validate user input. Their guess must be 1 letter long.
             while len(response) != 1:
                 response = input("Invalid input. What is your guess? >")
@@ -193,22 +165,29 @@ class Game:
                 # if word is complete, end game
                 if self.check_for_win(word, letters):
                     print(
-                        "Way to go! You won! The definition of + {id} is {self.definition[id]}"
+                        f"Way to go! You won! The definition of {self.questions[id]} is {self.definitions[id]}"
                     )
                     return
             else:
                 # admonish and show attempts remaining
                 self.failed_attempts += 1
                 # I need to figure out how to keep this message from printing when they get down to zero guesses remaining; show show that they lost if they get down to zero.
-                print(
-                    f"Sorry but you cannot pass go at this time. {self.attempts_allowed - self.failed_attempts} guesses remaining. Your progress so far: { self.show_word_so_far(word, letters) }"
-                )
+
                 if self.failed_attempts >= self.attempts_allowed:
                     # This is a Transformers reference XD
                     print(
-                        "You've failed me for the last time Starscream! The definition of + {id} is {self.definition[id]}"
+                        f"You've failed me for the last time Starscream! The definition of {self.questions[id]} is {self.definitions[id]}"
                     )
                     return
+                else:
+                    self.show_snake(self.failed_attempts)
+                    print(
+                        f"Sorry but you cannot pass go this time. {self.attempts_allowed - self.failed_attempts} guesses remaining. Your progress so far: { self.show_word_so_far(word, letters) }"
+                    )
+
+    def game_state(incorrect_answers):
+        if incorrect_answers == 1:
+            print
 
 
 g = Game()
