fde2dc9bdc2c114353f9af833e7f82d6f7d1a9b0
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-17 20:44:24 -0500

    work on the query and vercel

--- api/pygirl.py
+++ api/pygirl.py
@@ -61,8 +61,14 @@ class handler(BaseHTTPRequestHandler):
         # get initial query by targeting http:../pygirl/user-response
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
-        id = dic["id"]
-        message = play_turn(dic) #represents one round, this function will return a string
+        id_ = dic['id_']
+        user_answer = dic['user_answer']
+        guessed_letter = dic['letter']
+        if 'user_answer' in dic:
+            url = f'localhost:3000/api/pygirl/'
+            print(user_answer, id_)
+            message = user_answer + " " + id_ + " " + guessed_letter + " "
+        # message = play_turn(dic)  # represents one round, this function will return a string
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -72,172 +78,171 @@ class handler(BaseHTTPRequestHandler):
         # name = dic.get("id: 2")
         # does dict.get id:2 exist if so, yes, otherwise no
         # serverless function cannot have a counter, but you can incorporate counter here
-        # incorporate Vercel and Twillio
+        # incorporate Vercel and Twilio
         # make a request
         # wait for a response
         # respond
         # can have a URL for each turn, response, and game status
 
 
-
-class Game:
-    def __init__(self):
-        self.questions = {
-            1: "function",
-            2: "lambda",
-            3: "methods",
-            4: "tuple",
-            5: "bumbershoot",
-            6: "tree",
-            7: "recursion",
-            8: "class",
-            9: "object",
-            10: "stack",
-            11: "queue",
-            12: "kaggle",
-            13: "jupyter",
-            14: "pandas",
-            15: "enqueue",
-            16: "dequeue",
-            17: "serverless",
-            18: "automation",
-            19: "init",
-            20: "exception",
-        }
-        self.definitions = {
-            1: "function definition",
-            2: "lambda definition",
-            3: "methods definition",
-            4: "tuple definition",
-            5: "bumbershoot definition",
-            6: "tree definition",
-            7: "recursion definition",
-            8: "class definition",
-            9: "object definition",
-            10: "stack definition",
-            11: "queue definition",
-            12: "kaggle definition",
-            13: "jupyter definition",
-            14: "pandas definition",
-            15: "enqueue definition",
-            16: "dequeue definition",
-            17: "serverless definition",
-            18: "automation definition",
-            19: "init definition",
-            20: "exception definition",
-        }
-        self.attempts_allowed = 6
-        self.failed_attempts = 0
-
-    def show_word_so_far(self, word, letters):
-        # show the correctly guessed letters in the word
-        message = ""
-        for character in word:
-            if character in letters:
-                message += f"{character} "
-            # this part addes the underscore for the blank letters
-            else:
-                message += "_ "
-        return message
-
-    # this checks if they have enough words to win the game
-    def check_for_win(self, word, letters):
-        # show the correctly guessed letters in the word
-        correct_count = 0
-        for character in word:
-            if character in letters:
-                correct_count += 1
-        if correct_count == len(word):
-            return True
-        else:
-            return False
-
-    def show_snake(self, wrong_guesses):
-        snake_imgs = [
-            '''
-        "xxxx -=: xxxxx"''',
-            """
-                        ________
-                xxxx -=:___________  xxxxx""",
-            """
-                        ________/   /
-                xxxx -=:___________/ xxxxx""",
-            """
-                       \\
-                        \    /
-                ________/   /
-        xxxx -=:___________/ xxxxx""",
-            """
-                         _____
-                       /  0 0 \\
-                       \\
-                        \    /
-                ________/   /
-        xxxx -=:___________/ xxxxx""",
-            """
-                         _____
-                       /  0 0 \\
-                       \    --------<
-                        \    /
-                ________/   /
-        xxxx -=:___________/ xxxxx
-        """,
-        ]
-        print(snake_imgs[wrong_guesses])
-
-    def play(self):
-        # prompt to see if they want to play
-        print("Do you want to play a game?")
-        # I don't know how to do this for the serverless function. We can change this to make it work.
-        response = input("> ")
-        if response != "y":
-            print("Have a nice day!")
-            return
-        # pick random word. Their guess must be 1 letter long.
-        id = random.randint(1, len(self.questions))
-        word = self.questions[id]
-        print(self.show_word_so_far(word, set()))
-        # print(f"it's a secret: {word}")
-        letters = set()
-        # while game loop
-        while True:
-            response = input("\n What is your guess? >")
-            # validate user input. Their guess must be 1 letter long.
-            while len(response) != 1:
-                response = input("Invalid input. What is your guess? >")
-            letters.add(response)
-            if response in word:
-                # congratulate and show current progress
-                print(
-                    f"Yay! You got one. Your progress so far: { self.show_word_so_far(word, letters) }"
-                )
-                # if word is complete, end game
-                if self.check_for_win(word, letters):
-                    print(
-                        f"Way to go! You won! The definition of {self.questions[id]} is {self.definitions[id]}"
-                    )
-                    return
-            else:
-                # admonish and show attempts remaining
-                self.failed_attempts += 1
-                # I need to figure out how to keep this message from printing when they get down to zero guesses remaining; show show that they lost if they get down to zero.
-
-                if self.failed_attempts >= self.attempts_allowed:
-                    # This is a Transformers reference XD
-                    print(
-                        f"You've failed me for the last time Starscream! The definition of {self.questions[id]} is {self.definitions[id]}"
-                    )
-                    return
-                else:
-                    self.show_snake(self.failed_attempts)
-                    print(
-                        f"Sorry but you cannot pass go this time. {self.attempts_allowed - self.failed_attempts} guesses remaining. Your progress so far: { self.show_word_so_far(word, letters) }"
-                    )
-
-    def game_state(incorrect_answers):
-        if incorrect_answers == 1:
-            print
-
-
-g = Game()
-g.play()
+# class Game:
+#     def __init__(self):
+#         self.questions = {
+#             1: "function",
+#             2: "lambda",
+#             3: "methods",
+#             4: "tuple",
+#             5: "bumbershoot",
+#             6: "tree",
+#             7: "recursion",
+#             8: "class",
+#             9: "object",
+#             10: "stack",
+#             11: "queue",
+#             12: "kaggle",
+#             13: "jupyter",
+#             14: "pandas",
+#             15: "enqueue",
+#             16: "dequeue",
+#             17: "serverless",
+#             18: "automation",
+#             19: "init",
+#             20: "exception",
+#         }
+#         self.definitions = {
+#             1: "function definition",
+#             2: "lambda definition",
+#             3: "methods definition",
+#             4: "tuple definition",
+#             5: "bumbershoot definition",
+#             6: "tree definition",
+#             7: "recursion definition",
+#             8: "class definition",
+#             9: "object definition",
+#             10: "stack definition",
+#             11: "queue definition",
+#             12: "kaggle definition",
+#             13: "jupyter definition",
+#             14: "pandas definition",
+#             15: "enqueue definition",
+#             16: "dequeue definition",
+#             17: "serverless definition",
+#             18: "automation definition",
+#             19: "init definition",
+#             20: "exception definition",
+#         }
+#         self.attempts_allowed = 6
+#         self.failed_attempts = 0
+#
+#     def show_word_so_far(self, word, letters):
+#         # show the correctly guessed letters in the word
+#         message = ""
+#         for character in word:
+#             if character in letters:
+#                 message += f"{character} "
+#             # this part addes the underscore for the blank letters
+#             else:
+#                 message += "_ "
+#         return message
+#
+#     # this checks if they have enough words to win the game
+#     def check_for_win(self, word, letters):
+#         # show the correctly guessed letters in the word
+#         correct_count = 0
+#         for character in word:
+#             if character in letters:
+#                 correct_count += 1
+#         if correct_count == len(word):
+#             return True
+#         else:
+#             return False
+#
+#     def show_snake(self, wrong_guesses):
+#         snake_imgs = [
+#             '''
+#         "xxxx -=: xxxxx"''',
+#             """
+#                         ________
+#                 xxxx -=:___________  xxxxx""",
+#             """
+#                         ________/   /
+#                 xxxx -=:___________/ xxxxx""",
+#             """
+#                        \\
+#                         \    /
+#                 ________/   /
+#         xxxx -=:___________/ xxxxx""",
+#             """
+#                          _____
+#                        /  0 0 \\
+#                        \\
+#                         \    /
+#                 ________/   /
+#         xxxx -=:___________/ xxxxx""",
+#             """
+#                          _____
+#                        /  0 0 \\
+#                        \    --------<
+#                         \    /
+#                 ________/   /
+#         xxxx -=:___________/ xxxxx
+#         """,
+#         ]
+#         print(snake_imgs[wrong_guesses])
+#
+#     def play(self):
+#         # prompt to see if they want to play
+#         print("Do you want to play a game?")
+#         # I don't know how to do this for the serverless function. We can change this to make it work.
+#         response = input("> ")
+#         if response != "y":
+#             print("Have a nice day!")
+#             return
+#         # pick random word. Their guess must be 1 letter long.
+#         id = random.randint(1, len(self.questions))
+#         word = self.questions[id]
+#         print(self.show_word_so_far(word, set()))
+#         # print(f"it's a secret: {word}")
+#         letters = set()
+#         # while game loop
+#         while True:
+#             response = input("\n What is your guess? >")
+#             # validate user input. Their guess must be 1 letter long.
+#             while len(response) != 1:
+#                 response = input("Invalid input. What is your guess? >")
+#             letters.add(response)
+#             if response in word:
+#                 # congratulate and show current progress
+#                 print(
+#                     f"Yay! You got one. Your progress so far: { self.show_word_so_far(word, letters) }"
+#                 )
+#                 # if word is complete, end game
+#                 if self.check_for_win(word, letters):
+#                     print(
+#                         f"Way to go! You won! The definition of {self.questions[id]} is {self.definitions[id]}"
+#                     )
+#                     return
+#             else:
+#                 # admonish and show attempts remaining
+#                 self.failed_attempts += 1
+#                 # I need to figure out how to keep this message from printing when they get down to zero guesses remaining; show show that they lost if they get down to zero.
+#
+#                 if self.failed_attempts >= self.attempts_allowed:
+#                     # This is a Transformers reference XD
+#                     print(
+#                         f"You've failed me for the last time Starscream! The definition of {self.questions[id]} is {self.definitions[id]}"
+#                     )
+#                     return
+#                 else:
+#                     self.show_snake(self.failed_attempts)
+#                     print(
+#                         f"Sorry but you cannot pass go this time. {self.attempts_allowed - self.failed_attempts} guesses remaining. Your progress so far: { self.show_word_so_far(word, letters) }"
+#                     )
+#
+#     def game_state(incorrect_answers):
+#         if incorrect_answers == 1:
+#             print
+#
+#
+# g = Game()
+# g.play()
