fa20cab8c6e45a37eebe0b3748a7d983c5b08ef0
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-18 13:10:10 -0700

    functions

--- api/pygirl2.py
+++ api/pygirl2.py
@@ -1,7 +1,7 @@
+from mimetypes import guess_all_extensions
 import sys
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-
 # import requests
 from collections import Counter
 import random
@@ -51,63 +51,82 @@ f _ _ _ _ _ _ _
 Incorrect guesses: 
 Guesses left: 6
 '''
-# add underscore to ids
-def game_turn(id, guessed_letter, used_letters):
-  output = f"Game {id}"
-  correct = is_correct_letter(id, guessed_letter)
+def game_turn(id_, guessed_letter, used_letters):
+  output = f"Game {id_}"
+  correct = is_correct_letter(id_, guessed_letter)
   if correct:
-    output += "\nYou're correct!"
+    output += "\nYou're correct! \n"
   else:
-    output += "\nloser"
-  # word_in_progress = get_word_in_progress()
-  # incorrect_guesses = get_incorrect_guesses()
-  # guesses_left = get_guesses_left()
-  return output
+    output += "\n You've guessed an incorrect letter, \n Try Again! \n"
+
+  word_in_progress = get_word_in_progress(id_, used_letters)
+  incorrect_guesses = get_incorrect_guesses(id_, used_letters)
+  guesses_left = get_guesses_left(used_letters)
+
+  return output + word_in_progress + incorrect_guesses + guesses_left
 
 
-def get_word_in_progress():
-  message = ""
-  for character in word:
-    if character in letters:
-        message += f"{character} "
-
-def get_incorrect_guesses():
-  pass
-
-def get_guesses_left():
-  pass
-
-def is_correct_letter(id, guessed_letter):
-  word = words[id]
+def is_correct_letter(id_, guessed_letter):
+  #is the letter they guessed in the word to solve
+  word = words[id_]
   if guessed_letter in word:
     return True
   else:
     return False
 
-def start_game():
 
-  id = 1
-  word = words[id]
+def get_word_in_progress(id_, guessed_letter):
+  # show the correctly guessed letters in the word
+  word = words[id_]
+  message = ""
+  for guessed_letter in word:
+    if guessed_letter in word:
+      message += f"{guessed_letter}"
+    else:
+      message += "_ "
+    return f"{message}\n"
+
+def get_incorrect_guesses(id_, guessed_letter):
+  word = words[id_]
+  incorrect_letters = ""
+  message = ""
+  for character in guessed_letter:
+    if character not in word:
+      message += f"{character} "
+      incorrect_letters += incorrect_letters
+  return message + incorrect_letters
+
+def get_guesses_left(get_incorrect_guesses):
+  guesses = int(len(get_incorrect_guesses))
+  max_attempts = 10
+  guesses_left = max_attempts - guesses
+  return f" Attempts left: {guesses_left}"
+    
+
+def start_game():
+  id_ = 1
+  word = words[id_]
   unsolved_word = ""
   for character in word:
     unsolved_word += "_ "
   tries_left = 6
-  return f''' Game {id}
+  return f''' Game {id_}
 
 Guess a letter or solve the word
 
 {unsolved_word}
 
-Incorrect guesses: 
+Incorrect guesses: ***
+
 Guesses left: {tries_left}
 '''
 
 '''
-PyGirl.com/?id=1&letter=p&incorrect=lx
+PyGirl.com/?id_=1&letter=p&incorrect=lx
 '''
 
 if __name__ == "__main__":
-   response = game_turn(1, "x", "")
+   response = game_turn(id_=1, guessed_letter="u", used_letters="")
    print(response)
 
 
