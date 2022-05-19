524820ffe5b48e8596ba24e36803b138c4197077
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-19 13:17:16 -0700

    tests

--- tests/test_pygirl.py
+++ tests/test_pygirl.py
@@ -1,16 +1,48 @@
+from genericpath import exists
 import pytest
-from api.backend import Game
+from api.pygirl import start_game, get_guesses_left, get_incorrect_guesses, get_word_in_progress, game_turn
 
+def test_start_game_exists():
+    assert start_game
 
 def test_class_exists():
-    assert Game
+    assert game_turn
 
 
-def test_function_exists():
-    assert Game.welcome_message
-
-
-def test_input():
-    value = Game.welcome_message("y")
-    expected = "Let's play!"
+def test_starts_game():
+    value = start_game()['id']
+    value2 = start_game()['status']
+    expected = 1
+    status = "start"
+    print(value)
     assert value == expected
+    assert value2 == status
+
+#TODO: edgecase testing
+
+def test_starts():
+    value = start_game()['guesses']
+    expected = 0
+    assert value != expected
+
+# def test_no_more_word():
+#     value = get_incorrect_guesses[id_,"q", used]
+#     letter_guessed = "q"
+#     letter_guessed = "w"
+#     letter_guessed = "z"
+#     letter_guessed = "x"
+#     letter_guessed = "q"
+#     letter_guessed = "x"
+#     expected = 0
+#     assert value != expected
+
+
+# def test_game_data():
+#     TODO:test game_data when all code is completed
+#     id_ = game_data["id"]
+#     unsolved_word = game_data["working_word"]
+#     status = game_data["status"]
+#     guesses = game_data["guesses"]
+#     value = Game.welcome_message("y")
+#     expected = "Let's play!"
+#     assert value == expected
